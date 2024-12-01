93bb2d65d7135b26f9181757fe7a6edaf2081abd
Vincent Lo <vincentlo@greenaitech.com>
2024-12-01 23:37:53 +0800

    feat: add img2pdf

--- pdftools/cli.py
+++ pdftools/cli.py
@@ -1,4 +1,8 @@
+import os
+
 import click
+import img2pdf
+from PIL import Image
 from pypdf import PdfReader, PdfWriter
 
 from ._type import PagesType
@@ -25,6 +29,29 @@ def split(input_pdf, output, pages: list[int]):
         output_pdf.add_page(input_pdf.pages[int(page) - 1])
 
     output_pdf.write(output_pdf_path)
+
+
+@cli.command()
+@click.argument('image_path', type=click.Path(exists=True))
+@click.option('-o', '--output', type=click.Path(), required=False, default=None)
+def img_to_pdf(image_path, output):
+    """
+    Convert an image to a PDF.
+    """
+    if output is None:
+        output = os.path.splitext(image_path)[0] + '.pdf'
+
+    try:
+        with Image.open(image_path) as image:
+            pdf_bytes = img2pdf.convert(image.filename)
+
+        with open(output, 'wb') as pdf_file:
+            pdf_file.write(pdf_bytes)  # type: ignore
+
+        click.echo(f'Successfully converted image to PDF: {output}')
+
+    except Exception as e:
+        click.echo(f'Error converting image to PDF: {e}')
 
 
 if __name__ == '__main__':
